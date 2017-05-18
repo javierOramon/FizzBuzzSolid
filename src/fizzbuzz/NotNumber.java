dfc9c9bc584102ed61694304c1a4eb9f65a979e5
Javier Garcia <javier.garcia@oramon.nl>
2017-05-18 11:40:55 +0200

    Second test of TestNumberTest working properly

--- src/fizzbuzz/NotNumber.java
+++ src/fizzbuzz/NotNumber.java
@@ -5,6 +5,20 @@ public class NotNumber implements ISay {
 
     @Override
     public String say(String number) {
+        if(isNumber(number)) return "";
+
         return "Is not a number";
+    }
+
+    private boolean isNumber(String number) {
+        try
+        {
+            Integer.parseInt(number);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
     }
 }
